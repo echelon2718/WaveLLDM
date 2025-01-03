fd78b3e03f13e740a50f8c69932212e4a01ab4c4
Kevin Putra Santoso <92637327+echelon2718@users.noreply.github.com>
2025-01-03 20:04:01 +0700

    Update dataset.py

--- training/dataset.py
+++ training/dataset.py
@@ -9,13 +9,6 @@ from models.utils import ceil_mult8
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-def dynamic_range_compression(x, C=1, clip_val=1e-5):
-    return np.log(np.clip(x, a_min=clip_val, a_max=None) * C)
-
-
-def dynamic_range_decompression(x, C=1):
-    return np.exp(x) / C
-
 class MyDataset(Dataset):
     def __init__(
         self, data
@@ -28,10 +21,5 @@ class MyDataset(Dataset):
     def __getitem__(self, idx):
         x_i = torch.tensor(self.data[idx]["audio"]["array"]).unsqueeze(0).float()
         sr = self.data[idx]["audio"]["sampling_rate"]
-
-        if x_i.shape[1] < 22600:
-            pad = torch.zeros(x_i.shape[0], 22600)
-            pad[:x_i.shape[0], :x_i.shape[1]] = sample_input
-            x_i = pad
             
         return x_i.to(device), sr
