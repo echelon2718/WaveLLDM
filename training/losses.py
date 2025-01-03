4c51dbaa6033ea14605478c711a8b19bb3fff295
Kevin Putra Santoso <92637327+echelon2718@users.noreply.github.com>
2025-01-03 20:04:26 +0700

    Update losses.py

--- training/losses.py
+++ training/losses.py
@@ -2,8 +2,65 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
+from typing import List
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
+
+def feature_loss(fmap_r: List[torch.Tensor], fmap_g: List[torch.Tensor]):
+    loss = 0
+    for dr, dg in zip(fmap_r, fmap_g):
+        for rl, gl in zip(dr, dg):
+            rl = rl.float().detach()
+            gl = gl.float()
+            loss += torch.mean(torch.abs(rl - gl))
+
+    return loss * 2
+
+
+def discriminator_loss(
+    disc_real_outputs: List[torch.Tensor], disc_generated_outputs: List[torch.Tensor]
+):
+    loss = 0
+    r_losses = []
+    g_losses = []
+    for dr, dg in zip(disc_real_outputs, disc_generated_outputs):
+        dr = dr.float()
+        dg = dg.float()
+        r_loss = torch.mean((1 - dr) ** 2)
+        g_loss = torch.mean(dg**2)
+        loss += r_loss + g_loss
+        r_losses.append(r_loss.item())
+        g_losses.append(g_loss.item())
+
+    return loss, r_losses, g_losses
+
+
+def generator_loss(disc_outputs: List[torch.Tensor]):
+    loss = 0
+    gen_losses = []
+    for dg in disc_outputs:
+        dg = dg.float()
+        l = torch.mean((1 - dg) ** 2)
+        gen_losses.append(l)
+        loss += l
+
+    return loss, gen_losses
+
+def melspec_loss(aud_x, aud_y, spec_transform):
+    # Ensure same length in temporal dimension
+    aud_x, aud_y = aud_x.to(device), aud_y.to(device)
+    if aud_x.shape[-1] > aud_y.shape[-1]:
+        aud_y = F.pad(aud_y, (0, aud_x.shape[-1] - aud_y.shape[-1]))
+    elif aud_x.shape[-1] < aud_y.shape[-1]:
+        aud_x = F.pad(aud_x, (0, aud_y.shape[-1] - aud_x.shape[-1]))
+    
+    # Compute mel-spectrograms
+    spec_x = spec_transform(aud_x)
+    spec_y = spec_transform(aud_y)
+    
+    # Calculate L1 loss
+    loss = nn.L1Loss()(spec_x, spec_y)
+    return loss
 
 class AdversarialLoss(nn.Module):
     def __init__(self, device=device):
@@ -38,16 +95,11 @@ class FeatureMatchingLoss(nn.Module):
 class GeneratorLoss(nn.Module):
     def __init__(self,
                  fm_disc,
-                 lambda_adv=0.1, 
-                 lambda_recon=0.1, 
-                 lambda_fm=0.1, 
-                 lambda_kl=0.1, 
+                 lambda_adv=1, 
+                 lambda_fm=2, 
+                 lambda_spec=45, 
         ):
         super(GeneratorLoss, self).__init__()
-        self.adversarial_loss = AdversarialLoss()
-        self.recon_loss = nn.L1Loss()
-        self.feature_matching = FeatureMatchingLoss(disc=fm_disc)
-
         self.lambda_adv = lambda_adv
         self.lambda_recon = lambda_recon
         self.lambda_fm = lambda_fm
